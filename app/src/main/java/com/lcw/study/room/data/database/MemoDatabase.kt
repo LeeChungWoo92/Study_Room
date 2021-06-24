10f51f36511531d2cdaf939ca09d1d02258707e4
LeeChungWoo <chung92@rencar.co.kr>
2021-06-24 23:50:31 +0900

    add insert memo data

--- app/src/main/java/com/lcw/study/room/data/database/MemoDatabase.kt
+++ app/src/main/java/com/lcw/study/room/data/database/MemoDatabase.kt
@@ -1,11 +1,37 @@
 package com.lcw.study.room.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.lcw.study.room.data.dao.MemoDao
 import com.lcw.study.room.data.model.Memo
 
 @Database(entities = [Memo::class], version = 1)
-abstract class MemoDatabase: RoomDatabase() {
+abstract class MemoDatabase : RoomDatabase() {
     abstract fun memoDao(): MemoDao
+
+
+    companion object {
+        private var instance: MemoDatabase? = null
+
+
+        @Synchronized
+        fun getInstance(context: Context): MemoDatabase {
+            if (instance == null) {
+                synchronized(MemoDatabase::class.java) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MemoDatabase::class.java,
+                        "memo_database"
+
+                    ).build()
+                }
+
+            }
+            return instance!!
+
+        }
+    }
 }
+
---
+++ app/src/main/java/com/lcw/study/room/utils/DateTime.kt
@@ -0,0 +1,24 @@
+package com.lcw.study.room.utils
+
+import android.os.Build
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+object DateTime {
+
+    /** 현재날짜 구하기 **/
+    fun dateNow() : String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+             val localDate = LocalDate.now()
+             localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+         } else {
+             val now: Long = System.currentTimeMillis()
+             val mDate = Date(now)
+             val simpleDate = SimpleDateFormat("yyyy-MM-dd")
+             simpleDate.format(mDate)
+         }
+    }
+
+}
